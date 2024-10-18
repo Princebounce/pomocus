5af9d13e6bab2cb346d90aa542b2c05cd2556f95
Lawal Taofik <layomide81@gmail.com>
2024-10-18 01:35:21 +0100

    wrapped dashboard in privateroute

--- src/App.js
+++ src/App.js
@@ -3,14 +3,31 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Pomocus from "./components/Pomocus/Pomocus";
 import Home from "./components/Home/Home";
+import Login from "./application/Nav/User/Login";
+import Signup from "./application/Nav/User/Signup";
+import ForgotPassword from "./application/Nav/User/ForgotPassword";
+import { AuthProvider } from "./application/AuthContext";
+import PrivateRoute from "././application/PrivateRoute";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/newpage" element={<Pomocus />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/newpage"
+            element={
+              <PrivateRoute>
+                <Pomocus />
+              </PrivateRoute>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
